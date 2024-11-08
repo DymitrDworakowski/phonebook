e870087e2dab1fba6cad41e3c80f67a655d2e0fc
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2024-11-08 11:20:30 +0100

    Update Filter.jsx

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -29,9 +29,9 @@ const Filter = ({ handleChange, sortBy }) => {
         className={css.select_filter}
       >
         <option value="none">None</option>
-        <option value="byAB">Sort name by A-B</option>
-        <option value="byBA">Sort name by B-A</option>
-        <option value="byFavorite">Sort favorite</option>
+        <option value="byAB">Sort name A to Z</option>
+        <option value="byBA">Sort name Z to A</option>
+        <option value="byFavorite">Sort by favorite</option>
       </select>
     </div>
   );
